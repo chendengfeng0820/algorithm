32cc5bd320021be6d4ae5b48bd48f56ea30c0299
chendengfeng0820 <18790120405@163.com>
2020-07-27 13:55:39 +0800

    单链表

--- src/数据结构/链表/Demo.java
+++ src/数据结构/链表/Demo.java
@@ -14,12 +14,25 @@ public class Demo {
 		Node node2 = new Node(2,"bbb");
 		Node node3 = new Node(3,"ccc");
 		Node node4 = new Node(4,"ddd");
+		Node node5 = new Node(3,"fff");
 
-		list.add(node1);
-		list.add(node2);
-		list.add(node3);
-		list.add(node4);
+//		list.add(node1);
+//		list.add(node4);
+//		list.add(node2);
+//		list.add(node3);
+		list.addByOrder(node1);
+		list.addByOrder(node2);
+		list.addByOrder(node3);
+		list.addByOrder(node4);
 
+		list.list();
+
+//		System.out.println("修改后的=====");
+//		list.update(node5);
+//		list.list();
+
+		System.out.println("删除后的======");
+		list.delete(node3);
 		list.list();
 	}
 }
--- src/数据结构/链表/LinkedList.java
+++ src/数据结构/链表/LinkedList.java
@@ -10,6 +10,10 @@ public class LinkedList {
 
 	Node head = new Node(0,"");
 
+	/**
+	 * 按照list添加顺序添加
+	 * @param node
+	 */
 	public void add(Node node){
 		//temp节点指向head节点
 		Node temp = head;
@@ -25,6 +29,91 @@ public class LinkedList {
 		temp.next = node;
 	}
 
+	/**
+	 * 按照编号顺序添加
+	 */
+	public void addByOrder(Node node){
+		Node temp = head;
+		boolean flag = true;
+		if (head.next == null){
+			System.out.println("链表为空");
+		}
+		while (true){
+			if (temp.next == null){
+				break;
+			}
+			if (node.no < temp.next.no){
+				break;
+			}else if (node.no == temp.next.no){
+				flag = false ;
+				break;
+			}
+			temp = temp.next;
+		}
+		if (flag == false){
+			System.out.println("存在此节点" + temp.no);
+		}else {
+			node.next = temp.next;
+			temp.next = node;
+		}
+	}
+
+	/**
+	 * 根据编号修改节点
+	 * @param node
+	 */
+	public void update(Node node){
+		boolean flag = false;
+		Node temp = head;
+		if (head.next == null){
+			System.out.println("链表为空");
+		}
+		while (true){
+			if (temp.next == null){
+				break;
+			}
+			if (node.no == temp.no){
+				flag = true;
+				break;
+			}
+			temp = temp.next;
+		}
+		if (flag == true){
+			temp.name = node.name;
+		}else {
+			System.out.println("不存在此节点");
+		}
+	}
+
+	/**
+	 * 删除
+	 * @param node
+	 */
+	public void delete(Node node){
+		boolean flag = false;
+		Node temp = head;
+		if (head.next == null){
+			System.out.println("链表为空");
+		}
+		while (true){
+			if (temp.next == null){
+				break;
+			}
+			if (node.no == temp.next.no){
+				flag = true;
+				break;
+			}
+			temp = temp.next;
+		}
+		if (flag == true){
+			temp.next = temp.next.next;
+		}else {
+			System.out.println("不存在此节点");
+		}
+	}
+	/**
+	 * 遍历展示
+	 */
 	public void list(){
 		Node temp = head;
 		if (head.next == null){
